6f63f98460ad3bbf1b93c23494992481337dbdc0
izzetxanim@mail.ru <izzetxanim@mail.ru>
2020-02-16 23:57:11 +0400

    Project finished

--- Library/Library/Forms/ExportForm.cs
+++ Library/Library/Forms/ExportForm.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Library.Data;
 using Library.Models;
 using ClosedXML.Excel;
+using System.IO;
 
 namespace Library.Forms
 {
@@ -82,36 +83,34 @@ namespace Library.Forms
         //Excel-e export etmek
         private void BtnExcelExport_Click(object sender, EventArgs e)
         {
-
-            if (DgvReportOrders.Rows.Count > 0)
+        if(DgvReportOrders.Rows.Count > 0)
             {
-
                 try
                 {
                     var workbook = new XLWorkbook();
-                    var workSheet = workbook.Worksheets.Add("Bills");
+                    var workSheet = workbook.Worksheets.Add("Bills Sheet");
                     for (int i = 1; i < DgvReportOrders.Columns.Count; i++)
-                    {
+                    {                       
                         workSheet.Cell(1, i).SetValue(DgvReportOrders.Columns[i].HeaderText);
                         workSheet.Column(i).Width = 15;
                     }
-                    for (int i = 1; i < DgvReportOrders.Columns.Count - 1; i++)
+                    for(int i = 1; i < DgvReportOrders.Columns.Count -1; i++)
                     {
-                        for (int j = 1; j < DgvReportOrders.Columns.Count; j++)
+                        for(int j = 1; j < DgvReportOrders.Columns.Count; j++)
                         {
                             workSheet.Cell(i + 2, j).SetValue(DgvReportOrders.Rows[i].Cells[j].Value.ToString());
                         }
                     }
                     workbook.SaveAs(@"C:\Users\nrzayeva\Desktop\Bills.xlsx");
-                    MessageBox.Show("Excell file was created");
+                    MessageBox.Show("Excel file was created.");
                 }
                 catch (System.IO.IOException)
                 {
-
-                    MessageBox.Show("This file is open at the moment, please try again after closing the file.");
-                    return;
+                    MessageBox.Show("Please close the file and try aagain.");
                 }
             }
+           
         }
     }
 }
+
--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -17,7 +17,7 @@ namespace Library
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ExportForm());
+            Application.Run(new LoginForm());
         }
     }
 }
